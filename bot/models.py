e883e7383e400f518707956a5afd86df0b0a16a0
mirsaid-mirzohidov <mirzohidovm8@gmail.com>
2021-06-05 21:01:17 +0500

    Changed more files

--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,31 @@
-from django.db import models
+from django.db import models as db
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(db.Model):
+    name = db.CharField("Category name", max_length=30)
+
+    def __str__(self) -> str:
+        return self.name
+    
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+class Article(db.Model):
+    title = db.CharField("Article title", max_length=80)
+    text = db.TextField("Article body")
+    category = db.ForeignKey(Category, verbose_name="Category", on_delete=db.CASCADE)
+    author = db.ForeignKey(
+        User, verbose_name="Author", on_delete=db.CASCADE
+    )
+    posted_date = db.DateTimeField("Posted date", default=timezone.now())
+    edited_date = db.DateTimeField("Edited date", default=timezone.now())
+
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        verbose_name = "Article"
+        verbose_name_plural = "Articles"
