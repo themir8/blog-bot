239de74eb998a68ca0ab624086c7ae917e0ddcf9
mirsaid <mirzohidovm8@gmail.com>
2021-07-01 09:16:31 +0500

    null=True for name forms

--- users/models.py
+++ users/models.py
@@ -5,8 +5,8 @@ from django.utils import timezone
 class BotUser(db.Model):
     user_id = db.IntegerField("User telegram id", null=False, unique=True)
     username = db.CharField("Username", max_length=32, unique=True)
-    first_name = db.CharField("First name", max_length=15)
-    last_name = db.CharField("Last name", max_length=15)
+    first_name = db.CharField("First name", max_length=15, null=True)
+    last_name = db.CharField("Last name", max_length=15, null=True)
     phone = db.CharField("Phone number", max_length=12)
     date_joined = db.DateTimeField('Date joined', default=timezone.now)
     is_active = db.BooleanField("User is active?", default=True)
