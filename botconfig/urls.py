880e3f9b0590576a0753abcc8bd4e684f0639d69
mirsaid-mirzohidov <mirzohidovm8@gmail.com>
2021-07-14 15:19:41 +0500

    Delete all backend files

--- botconfig/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('api/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('admin/', admin.site.urls),
-    path('api/', include('bot.urls')),
-]
