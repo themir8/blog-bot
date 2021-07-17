20161d2eb86ac8124e9199f8bd6404c27ea59696
mirsaid-mirzohidov <mirzohidovm8@gmail.com>
2021-07-17 18:52:10 +0500

    Add handler to inline mode queries

--- bot/main.py
+++ bot/main.py
@@ -1,18 +1,16 @@
-from bot.buttons import article_menu
 import os
-import logging
 
 import dotenv
 from aiogram import Bot, Dispatcher
 
-from .handlers import start_handler, text_handler, article_handler
-
+from bot.logger import logger_init
+from .handlers import article_handler, inline_get_article, start_handler, text_handler
 
 # Load dotenv
 dotenv.load_dotenv()
 
 # Configure logging
-logging.basicConfig(level=logging.INFO)
+logger_init("INFO")
 
 
 async def main():
@@ -29,6 +27,7 @@ async def main():
         dp.register_message_handler(
             text_handler, content_types="text")
         dp.register_callback_query_handler(article_handler)
+        dp.register_inline_handler(inline_get_article)
         await dp.start_polling()
     finally:
         await bot.close()
