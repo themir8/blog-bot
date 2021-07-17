6d1091136ab78fbb98fe498324b9712544e663b3
mirsaid-mirzohidov <mirzohidovm8@gmail.com>
2021-07-17 18:48:43 +0500

    Add get all articles class

--- bot/views.py
+++ bot/views.py
@@ -13,79 +13,87 @@ from . import serializers
 
 class UserCreateView(APIView):
 
-	def get(self, request):
-		serializer = serializers.UserCreateSerializer()
-		return Response(serializer.data)
+    def get(self, request):
+        serializer = serializers.UserCreateSerializer()
+        return Response(serializer.data)
 
-	def post(self, request):
-		try:
-			user = serializers.UserCreateSerializer(data=request.data)
-			if BotUser.objects.filter(user_id=request.data["user_id"]).exists():
-				return Response(status=status.HTTP_200_OK)
-			elif not BotUser.objects.filter(user_id=request.data["user_id"]).exists() and user.is_valid():
-				user.save()
-				return Response(status=status.HTTP_201_CREATED)
-		except Exception as e:
-			print(e)
-			return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    def post(self, request):
+        try:
+            user = serializers.UserCreateSerializer(data=request.data)
+            if BotUser.objects.filter(user_id=request.data["user_id"]).exists():
+                return Response(status=status.HTTP_200_OK)
+            elif not BotUser.objects.filter(user_id=request.data["user_id"]).exists() and user.is_valid():
+                user.save()
+                return Response(status=status.HTTP_201_CREATED)
+        except Exception as e:
+            print(e)
+            return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
 class GetBlogView(UpdateAPIView):
-	serializer_class = serializers.BlogGetSerializer
+    serializer_class = serializers.BlogGetSerializer
 
-	def get_queryset(self):
-		return Blog.objects.all()
+    def get_queryset(self):
+        return Blog.objects.all()
 
-	def get(self, request, pk):
-		try:
-			blog = Blog.objects.get(owner__user_id=pk)
-			serializer = serializers.BlogGetSerializer(blog)
-			return Response(serializer.data)
-		except:
-			return Response({'status': 'Not found'}, status=status.HTTP_404_NOT_FOUND)
+    def get(self, request, pk):
+        try:
+            blog = Blog.objects.get(owner__user_id=pk)
+            serializer = serializers.BlogGetSerializer(blog)
+            return Response(serializer.data)
+        except:
+            return Response({'status': 'Not found'}, status=status.HTTP_404_NOT_FOUND)
 
-	def put(self, request, *args, **kwargs):
-		return self.update(request, *args, **kwargs)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
 
 class CreateBlogView(APIView):
 
-	serializer_class = serializers.BlogCreateSerializer
+    serializer_class = serializers.BlogCreateSerializer
 
-	def get(self, request):
-		serializer = serializers.BlogCreateSerializer()
-		return Response(serializer.data)
+    def get(self, request):
+        serializer = serializers.BlogCreateSerializer()
+        return Response(serializer.data)
 
-	def post(self, request):
-		blog = serializers.BlogCreateSerializer(data=request.data)
-		owner = Blog.objects.get(owner__id=request.POST.get("owner"))
-		if owner:
-			data = {"detail": "У вас уже есть свой блог!"}
-			return Response(data, status=404)
-		else:
-			if blog.is_valid():
-				blog.save()
-			return Response(status=201)
+    def post(self, request):
+        blog = serializers.BlogCreateSerializer(data=request.data)
+        owner = Blog.objects.get(owner__id=request.POST.get("owner"))
+        if owner:
+            data = {"detail": "У вас уже есть свой блог!"}
+            return Response(data, status=404)
+        else:
+            if blog.is_valid():
+                blog.save()
+            return Response(status=201)
 
 
 class CreateBlogSubscribersView(APIView):
 
-	def post(self, request):
-		blog = serializers.BlogSubscribersCreateSerializer(data=request.data)
-		if blog.is_valid():
-			blog.save()
-		return Response(status=201)
+    def post(self, request):
+        blog = serializers.BlogSubscribersCreateSerializer(data=request.data)
+        if blog.is_valid():
+            blog.save()
+        return Response(status=201)
 
 
 class ArticleGetAPIView(GenericAPIView, UpdateModelMixin):
 
-	queryset = Article.objects.all()
-	serializer_class = serializers.ArticleGetSerializer
+    queryset = Article.objects.all()
+    serializer_class = serializers.ArticleGetSerializer
 
-	def get(self, request, pk):
-		article = Article.objects.get(id=pk)
-		serializer = serializers.ArticleGetSerializer(article)
-		return Response(serializer.data)
+    def get(self, request, pk):
+        article = Article.objects.get(id=pk)
+        serializer = serializers.ArticleGetSerializer(article)
+        return Response(serializer.data)
 
-	def put(self, request, *args, **kwargs):
-		return self.update(request, *args, **kwargs)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+
+class ArticlesAPIView(APIView):
+
+    def get(self, request):
+        articles = Article.objects.all().order_by("-posted_date")
+        serializer = serializers.ArticleGetSerializer(articles, many=True)
+        return Response(serializer.data)
