3d2a68fc4a17cf45d9e277245e8f85cd8dca1d75
mirsaid-mirzohidov <mirzohidovm8@gmail.com>
2021-07-17 11:12:46 +0500

    Main and article menu buttons

---
+++ bot/buttons.py
@@ -0,0 +1,31 @@
+from aiogram import types
+from aiogram.types.reply_keyboard import ReplyKeyboardMarkup
+
+from .models import Article
+
+
+def main_btn(custom: str) -> ReplyKeyboardMarkup:
+    """Main button"""
+    buttons = ["⭐️ Рекомендации",
+               "👤 Подписки",
+               "🔍 Поиск"]
+
+    # If custom is exists add to buttons list
+    if custom:
+        buttons.append(custom)
+
+    menu = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    menu.add(*buttons)
+    return menu
+
+
+def article_menu(articles: list[Article]) -> types.InlineKeyboardMarkup:
+    """Article menu"""
+    menu = types.InlineKeyboardMarkup(row_width=2)
+    buttons = []
+    for article in articles:
+        buttons.append(types.InlineKeyboardButton(
+            article.title, callback_data=article.id))
+    menu.add(*buttons)
+
+    return menu
