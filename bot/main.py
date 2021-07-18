6ee09ce2e4a7e50a2a6861cd7b9100f8685470d4
mirsaid-mirzohidov <mirzohidovm8@gmail.com>
2021-07-18 13:54:25 +0500

    Handler for search_function

--- bot/main.py
+++ bot/main.py
@@ -1,15 +1,19 @@
+import pathlib
+from bot.states import States
 import os
 
 import dotenv
 from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.files import PickleStorage
 
 from bot.logger import logger_init
-from .handlers import article_handler, inline_get_article, start_handler, text_handler
+from .handlers import article_handler, inline_get_article, search_function, start_handler, text_handler
 
 # Load dotenv
 dotenv.load_dotenv()
 
-# Configure logging
+# Configure logging.
+# 4-levels for logging: INFO, DEBUG, WARNING, ERROR
 logger_init("INFO")
 
 
@@ -19,13 +23,17 @@ async def main():
     # Initialize bot and dispatcher
     bot = Bot(token=os.getenv("API_TOKEN"))
     try:
-        dp = Dispatcher(bot)
+        storage = PickleStorage(pathlib.Path("db"))
+        dp = Dispatcher(bot, storage=storage)
         # start_handler register
         dp.register_message_handler(
-            start_handler, commands={"start"})
+            start_handler, commands={"start"}, state="*")
         # text_handler register
         dp.register_message_handler(
             text_handler, content_types="text")
+        # register search_function
+        dp.register_message_handler(
+            search_function, state=States.search)
         dp.register_callback_query_handler(article_handler)
         dp.register_inline_handler(inline_get_article)
         await dp.start_polling()
